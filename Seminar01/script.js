931a0c7efecacf2aae9ac07cf35f6ea8d3258b73
Valerii_Panov <val250640@mail.ru>
2024-01-26 14:15:38 +1000

    sem01

---
+++ Seminar01/script.js
@@ -0,0 +1,31 @@
+alert("Hello World");
+
+let num = 123;
+console.log(num);
+let a = 5;
+a = 7;
+console.log(a);
+let b = 1+2+3;
+console.log(b);
+
+let c = 10, d = 2;
+let sum = c + d;
+let dif = c - d;
+let multy = c*d;
+let sub = c/d;
+console.log(sum, dif, multy, sub);
+console.log(sum+dif+multy+sub);
+
+a = 1.5;
+b = 0.75;
+console.log(a+b);
+
+console.log(`${a}`);
+
+let firstName = "Valerii";
+let lastName = "Panov";
+console.log(firstName, lastName);
+
+let java = "Java";
+let script = "Script";
+console.log(java+script);
